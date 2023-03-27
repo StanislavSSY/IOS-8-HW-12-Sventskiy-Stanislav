842ebfb2f48f17e09a0253a34ffdcadb6f9bab36
StanislavSSY <stanislav.mifub@yandex.ru>
2023-03-28 00:05:33 +0700

    Add timerLabel.

--- IOS-8-HW-12-Sventskiy Stanislav/Sources/ViewController.swift
+++ IOS-8-HW-12-Sventskiy Stanislav/Sources/ViewController.swift
@@ -10,9 +10,54 @@ import SnapKit
 
 class ViewController: UIViewController {
 
+    var timer: Timer?
+    var timeLeft = 25
+    var isWorking = true
+    
+    // MARK: - UIElements
+    private var timerLabel: UILabel = {
+        let label = UILabel()
+        label.text = "00.25"
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 50, weight: .regular)
+        return label
+    }()
+    
+    private var labelStart: UILabel = {
+        let labelStart = UILabel()
+        
+        
+        return labelStart
+    }()
+    
+    
+    
+    // MARK: - LifeCicle
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBlue
+        setupView()
+        setupHierarchy()
+        setupLayout()
     }
+    
+    // MARK: - Setups
+    
+    
+    private func setupView() {
+        view.backgroundColor = .systemBlue
+        
+    }
+    
+    private func setupHierarchy() {
+        
+    }
+    
+    private func setupLayout() {
+        
+    }
+    
+    // MARK: - Actions
 }
 
